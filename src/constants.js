b533f2f920093008d6c374520a437ccebffe96b8
Garvansh <garvansh247@gmail.com>
2026-06-23 01:59:48 +0530

    feat: implement modular mongoose database connection with promise-driven server startup

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+const DB_NAME="video_db";
+
+export {DB_NAME};
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import connectToDatabase from "./db/index.js";
+
+const app=express();
+const PORT=process.env.PORT || 8000;
+
+connectToDatabase()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.error("Express server error:", error);
+        process.exit(1); // Exit the process with an error code
+    });
+
+    app.listen(PORT,()=>{
+        console.log(`Server is running on port ${PORT}`);
+    });
+})
+.catch((error)=>{
+    console.error("Error connecting to database:", error);
+    process.exit(1); // Exit the process with an error code
+});
+
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+});
